91d628007cdc90b627fd1943f52d99f0c984cae2
mailor <vvorlovv7799@gmail.com>
2026-07-05 01:58:31 +0300

    актуализация тестов после удаления html слоя

--- backend/mainpage/tests.py
+++ backend/mainpage/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 from django.urls import reverse
+
 from .models import AnimeDescription, Comment
+
+# Тесты, проверявшие рендер страниц и пагинацию через шаблоны, удалены вместе с HTML-слоем: UI переехал в frontend/. HTTP-слой будет покрыт тестами заново при переходе на django-ninja
 
 User = get_user_model()
 
@@ -55,30 +58,6 @@ class CommentModelTest(TestCase):
         self.assertEqual(self.anime.comments.first().user, self.user)
 
 
-class SteinsGatePageTest(TestCase):
-
-    def setUp(self):
-        self.client = Client()
-        self.url = reverse('steins_gate_page')
-
-    def test_page_loads_successfully(self):
-        response = self.client.get(self.url)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'steins-gate_page.html')
-
-    def test_page_contains_anime_title(self):
-        response = self.client.get(self.url)
-
-        self.assertContains(response, 'Steins;Gate')
-
-    def test_divergence_meter_present(self):
-        response = self.client.get(self.url)
-
-        self.assertContains(response, 'nixie-digit')
-        self.assertContains(response, '0.337187')
-
-
 class CommentFunctionalityTest(TestCase):
 
     def setUp(self):
@@ -120,33 +99,6 @@ class CommentFunctionalityTest(TestCase):
         self.assertEqual(Comment.objects.count(), 0)
 
 
-class PaginationTest(TestCase):
-
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username='test', password='pass')
-        self.anime = AnimeDescription.objects.create(name="Steins;Gate")
-
-        for i in range(10):
-            Comment.objects.create(
-                user=self.user,
-                anime=self.anime,
-                text=f"Коммент {i}"
-            )
-
-        self.url = reverse('steins_gate_page')
-
-    def test_first_page_has_6_comments(self):
-        response = self.client.get(self.url)
-
-        self.assertEqual(len(response.context['comments']), 6)
-
-    def test_second_page_has_remaining_comments(self):
-        response = self.client.get(f'{self.url}?page=2')
-
-        self.assertEqual(len(response.context['comments']), 4)
-
-
 class URLTest(TestCase):
 
     def test_steins_gate_url_resolves(self):
@@ -160,81 +112,3 @@ class URLTest(TestCase):
     def test_future_gadget_lab_url_resolves(self):
         url = reverse('future_gadget_laboratory')
         self.assertEqual(url, '/future-gadget-laboratory/')
-
-
-class AuthenticationTest(TestCase):
-
-    def test_user_can_register_with_email_verification(self):
-
-        response = self.client.post(reverse('register'), {
-            'username': 'kurisu',
-            'email': 'kurisu@gmail.com',
-            'password': 'complex_pass_123',
-            'confirm_password': 'complex_pass_123'
-        })
-
-        self.assertRedirects(response, reverse('email_verification'))
-
-        self.assertFalse(User.objects.filter(username='kurisu').exists())
-
-        self.assertIn('verification_code', self.client.session)
-        self.assertIn('registration_data', self.client.session)
-
-        code = self.client.session['verification_code']
-        response = self.client.post(reverse('email_verification'), {'code': str(code)})
-
-        self.assertTrue(User.objects.filter(username='kurisu').exists())
-
-        user = User.objects.get(username='kurisu')
-        self.assertEqual(int(self.client.session['_auth_user_id']), user.pk)
-
-        self.assertTrue(hasattr(user, 'profile'))
-        self.assertEqual(user.profile.nickname, 'kurisu')
-
-    def test_register_sends_email(self):
-        from django.core import mail
-
-        self.client.post(reverse('register'), {
-            'username': 'daru',
-            'email': 'daru@gmail.com',
-            'password': 'super_haker_123',
-            'confirm_password': 'super_haker_123'
-        })
-
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertIn('verification', mail.outbox[0].subject.lower())
-        self.assertEqual(mail.outbox[0].to, ['daru@gmail.com'])
-
-    def test_wrong_verification_code_fails(self):
-
-        self.client.post(reverse('register'), {
-            'username': 'mayuri',
-            'email': 'mayuri@gmail.com',
-            'password': 'tuturu_123',
-            'confirm_password': 'tuturu_123'
-        })
-
-        self.client.post(reverse('email_verification'), {
-            'code': '000000'
-        })
-
-        self.assertFalse(User.objects.filter(username='mayuri').exists())
-
-    def test_email_domain_validation(self):
-
-        response = self.client.post(reverse('register'), {
-            'username': 'test',
-            'email': 'test@blocked.com',
-            'password': 'pass_123',
-            'confirm_password': 'pass_123'
-        })
-
-        form = response.context['form']
-        self.assertIn('email', form.errors)
-
-    def test_user_can_login(self):
-        User.objects.create_user(username='daru', password='super_haker')
-
-        logged_in = self.client.login(username='daru', password='super_haker')
-
-        self.assertTrue(logged_in)
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,94 @@
+from django.contrib.auth import get_user_model
+from django.core import mail
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .forms import RegisterForm
+
+# Тесты, требовавшие рендера HTML-шаблонов (неверный код верификации, отображение ошибок формы), удалены вместе с HTML-слоем и вернутся при переходе на django-ninja
+
+User = get_user_model()
+
+
+class RegisterFormTest(TestCase):
+
+    def test_email_domain_validation(self):
+        form = RegisterForm(data={
+            'username': 'test',
+            'email': 'test@blocked.com',
+            'password': 'pass_123',
+            'confirm_password': 'pass_123',
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors)
+
+    def test_password_mismatch(self):
+        form = RegisterForm(data={
+            'username': 'test',
+            'email': 'test@gmail.com',
+            'password': 'complex_pass_123',
+            'confirm_password': 'different_pass_123',
+        })
+
+        self.assertFalse(form.is_valid())
+
+    def test_duplicate_email_rejected(self):
+        User.objects.create_user(username='taken', email='taken@gmail.com', password='x')
+
+        form = RegisterForm(data={
+            'username': 'newuser',
+            'email': 'taken@gmail.com',
+            'password': 'complex_pass_123',
+            'confirm_password': 'complex_pass_123',
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors)
+
+
+class AuthenticationTest(TestCase):
+
+    def test_user_can_register_with_email_verification(self):
+        response = self.client.post(reverse('register'), {
+            'username': 'kurisu',
+            'email': 'kurisu@gmail.com',
+            'password': 'complex_pass_123',
+            'confirm_password': 'complex_pass_123'
+        })
+
+        self.assertRedirects(
+            response, reverse('email_verification'), fetch_redirect_response=False
+        )
+        self.assertFalse(User.objects.filter(username='kurisu').exists())
+        self.assertIn('verification_code', self.client.session)
+        self.assertIn('registration_data', self.client.session)
+
+        code = self.client.session['verification_code']
+        self.client.post(reverse('email_verification'), {'code': str(code)})
+
+        self.assertTrue(User.objects.filter(username='kurisu').exists())
+
+        user = User.objects.get(username='kurisu')
+        self.assertEqual(int(self.client.session['_auth_user_id']), user.pk)
+        self.assertTrue(hasattr(user, 'profile'))
+        self.assertEqual(user.profile.nickname, 'kurisu')
+
+    def test_register_sends_email(self):
+        self.client.post(reverse('register'), {
+            'username': 'daru',
+            'email': 'daru@gmail.com',
+            'password': 'super_haker_123',
+            'confirm_password': 'super_haker_123'
+        })
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn('verification', mail.outbox[0].subject.lower())
+        self.assertEqual(mail.outbox[0].to, ['daru@gmail.com'])
+
+    def test_user_can_login(self):
+        User.objects.create_user(username='daru', password='super_haker')
+
+        logged_in = self.client.login(username='daru', password='super_haker')
+
+        self.assertTrue(logged_in)
